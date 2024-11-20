27057f11f5d9dbb1d6ed40402962dec129d83b8e
Rohankc073 <rohankc073@gmail.com>
2024-11-20 21:09:24 +0545

    Simple Intrest

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,12 +1,13 @@
-void main(){
-  
+void main() {
   print(calSimp(10000, 10, 2));
 }
 
-double calSimp(double principle,
-  double rate,
-  int time){
-return ((principle*rate*time)/100);
-
-
+double calSimp(double principle, double rate, int time) {
+  return ((principle * rate * time) / 100);
 }
+
+/*
+Named parameters: {}, Null-coalescing operator: ?? and String Interpolation: 
+Use $variable or ${expression} to format the output.
+*/
+//Named Parameter 
---
+++ bin/named_paremeter.dart
@@ -0,0 +1,13 @@
+void main() {
+  print(add(first: 1, second: 3, saru: 3, adarsh: 5));
+}
+
+int add({
+  required int? first, // Must provide a value when calling.
+  required int? second, // Must provide a value when calling.
+  int? saru, // Optional, can be null.
+  int? adarsh, // Optional, can be null.
+}) {
+  // Calculates the sum using the default value of 0 if any parameter is null.
+  return (first ?? 0) + (second ?? 0) + (saru ?? 0) + (adarsh ?? 0);
+}
