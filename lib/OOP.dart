6de69d9901a8482f2da90a50487c5ea64cf596c7
Rohankc073 <rohankc073@gmail.com>
2024-11-23 21:04:38 +0545

    OOP

--- lib/OOP.dart
+++ lib/OOP.dart
@@ -1,25 +1,48 @@
-// ignore: file_names
-void main() {
-  // Create an object of the Student class
-  Student s1 = Student("Rohan KC", 21, 200);
+// void main() {
+//   // Create an object of the Student class
+//   Student s1 = Student("Rohan KC", 21, 200);
 
-  // Call the instance method using the object
-  s1.showStudntInfo();
+//   // Call the instance method using the object
+//   s1.showStudntInfo();
+// }
+
+// class Student {
+//   // Class properties
+//   String name;
+//   int age;
+//   int rollNo;
+
+//   // Constructor
+//   Student(this.name, this.age, this.rollNo);
+
+//   // Instance method to show student information
+//   void showStudntInfo() {
+//     print("Student Name is : $name");
+//     print("Student age : $age");
+//     print("Student Roll No : $rollNo");
+//   }
+// }
+
+//Encapsulation : Hide the internal data of an object and only show what is necessary
+
+class BankAccount {
+  double _balance = 0;
+
+  //Method to deposit money
+  void deposit(double amount) {
+    if (amount > 0) {
+      _balance = _balance + amount;
+    }
+  }
+
+  //Method to check balance
+  double getbalance() {
+    return _balance;
+  }
 }
 
-class Student {
-  // Class properties
-  String name;
-  int age;
-  int rollNo;
-
-  // Constructor
-  Student(this.name, this.age, this.rollNo);
-
-  // Instance method to show student information
-  void showStudntInfo() {
-    print("Student Name is : $name");
-    print("Student age : $age");
-    print("Student Roll No : $rollNo");
-  }
+void main() {
+  BankAccount myAccount = BankAccount();
+  myAccount.deposit(500);
+  print("Balance: ${myAccount.getbalance()} ");
 }
