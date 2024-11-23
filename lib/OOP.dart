bb157458c06611a3ef46c11292be8f11fc50a1b9
Rohankc073 <rohankc073@gmail.com>
2024-11-23 10:28:34 +0545

    OOP concept

--- lib/OOP.dart
+++ lib/OOP.dart
@@ -1,2 +1,25 @@
+// ignore: file_names
+void main() {
+  // Create an object of the Student class
+  Student s1 = Student("Rohan KC", 21, 200);
 
-//OOP concept 
+  // Call the instance method using the object
+  s1.showStudntInfo();
+}
+
+class Student {
+  // Class properties
+  String name;
+  int age;
+  int rollNo;
+
+  // Constructor
+  Student(this.name, this.age, this.rollNo);
+
+  // Instance method to show student information
+  void showStudntInfo() {
+    print("Student Name is : $name");
+    print("Student age : $age");
+    print("Student Roll No : $rollNo");
+  }
+}
